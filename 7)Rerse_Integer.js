8966fd0c4db38ce0042bda71ad507716a2b47973
daveabebe12 <162504680+daveabebe12@users.noreply.github.com>
2024-11-12 23:28:39 +0300

    7th question edited

--- 7)Rerse_Integer.js
+++ 7)Rerse_Integer.js
@@ -1,4 +1,13 @@
-var reverse = (num) => {
-    if(num < 0) return -1 * reverse -num;
+var reverse = function (x) {
+  const INT_MIN = -Math.pow(2, 31);
+  const INT_MAX = Math.pow(2, 31) - 1;
+  const sign = x < 0 ? -1 : 1;
+  x = Math.abs(x);
+  const reversed_x = parseInt(x.toString().split("").reverse().join(""));
+  const result = reversed_x * sign;
+  if (result < INT_MIN || result > INT_MAX) {
+    return 0;
+  }
 
-}
+  return result;
+};
