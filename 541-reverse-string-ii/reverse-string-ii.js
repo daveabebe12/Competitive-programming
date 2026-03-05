954d771a9cd9fc18dbfff6a49329e1c0fdc54ca8
Dawit Abebe <162504680+daveabebe12@users.noreply.github.com>
2026-03-05 16:49:55 +0300

    Time: 5 ms (56.04%) | Memory: 57.3 MB (42.46%) - LeetSync

---
+++ 541-reverse-string-ii/reverse-string-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function(s, k) {
+    s = s.split("");
+    for(let i = 0; i < s.length; i += k*2){
+        let left = i;
+        let right = Math.min(i + k -1, s.length - 1);
+        while(left < right){
+            let temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    return s.join("");
+};
